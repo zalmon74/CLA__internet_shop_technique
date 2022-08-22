c90d0a323a1cee73936d73f3c3fe5263c01634b9
zalmon74 <zalmon74@yandex.ru>
2022-08-22 22:57:32 +0700

    Добавлена страница профиля пользователя. Добавлена возможность изменить данные профиля и пароль.

---
+++ CLA__shop/accounts/templatetags/accounts_tags.py
@@ -0,0 +1,28 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('accounts/show_tags/profile_menu.html')
+def show_profile_menu():
+    """
+    Тэг, который формирует меню в профиле пользователя
+    """
+
+    menu = [
+        {'name': 'Профиль', 'url_name': 'user_profile'},
+        {'name': 'Любимые товары', 'url_name': 'user_profile'},
+        {'name': 'Любимые бренды', 'url_name': 'user_profile'},
+        {'name': 'История покупок', 'url_name': 'user_profile'},
+    ]
+
+    return {'list_menu': menu}
+
+
+@register.inclusion_tag('accounts/show_tags/show_std_form.html')
+def show_std_form(form, title, text_button):
+    """
+    Тэг, который формирует отображение стандартного меню
+    """
+    return {'form': form, 'title': title, 'text_button': text_button, }
