df08bcea552e5e1ac699a29366f40d9d454bf3c9
zalmon74 <zalmon74@yandex.ru>
2023-01-14 16:31:12 +0700

    Добавлена возможность удаления товаров из корзины

--- CLA__shop/shopping_basket/urls.py
+++ CLA__shop/shopping_basket/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from django.views.decorators.cache import never_cache
 
-from .views import ShopingBasketView, ajax_add_product_in_shopping_bassket
+from .views import (
+    ShopingBasketView, ajax_add_product_in_shopping_bassket,
+    ajax_delete_product_from_shopping_bassket,
+)
 
 urlpatterns = [
     path('ajax_add_product_in_shopping_bassket/',
          never_cache(ajax_add_product_in_shopping_bassket),
          name='ajax_add_product_in_shopping_bassket'),
+    path('ajax_delete_product_from_shopping_bassket/',
+         never_cache(ajax_delete_product_from_shopping_bassket),
+         name='ajax_delete_product_from_shopping_bassket'),
     path('', never_cache(ShopingBasketView.as_view()), name='shopping_basket'),
 ]
--- CLA__shop/shopping_basket/views.py
+++ CLA__shop/shopping_basket/views.py
@@ -6,6 +6,8 @@ from .models import Product, ShoppingBasketModel
 
 
 def ajax_add_product_in_shopping_bassket(request):
+    """ Вьюшка, которая обрабатывает ajax-запрос на добавление товара в корзину
+    """
     product_id = int(request.GET['product_url_detail'].split('/')[-2])
     # Добавляем товар в корзину к пользователю
     ShoppingBasketModel.objects.update_or_create(
@@ -13,6 +15,14 @@ def ajax_add_product_in_shopping_bassket(request):
         product=Product.objects.get(id=product_id),
         count=1,
     )
+    return HttpResponse()
+
+
+def ajax_delete_product_from_shopping_bassket(request):
+    """ Вьюшка, которая обрабатывает ajax-запрос на удаление товара из корзины
+    """
+    product_id = int(request.GET['product_url_detail'].split('/')[-2])
+    ShoppingBasketModel.objects.get(user=request.user, product=product_id).delete()
     return HttpResponse()
 
 
